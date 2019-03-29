455b44e845ba9d35aade217ff8db6db7e254b345
avpa11 <alinavpak@gmail.com>
2019-03-28 23:03:00 -0700

    very simple calendar

--- api/User/admin.php
+++ api/User/admin.php
@@ -7,6 +7,7 @@ include_once '../user/reservation.php';
  
 $resev = new reservation();
 
+// function for html haed and nav
 function heading() { ?>
   
   <!DOCTYPE html>
@@ -20,6 +21,8 @@ function heading() { ?>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
   <link rel="stylesheet" href="../../assets/css/welcome.css" />
   <link rel="stylesheet" href="../../assets/css/reservation.css" />
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>  
   
 </head>
 <body>
@@ -33,18 +36,139 @@ function heading() { ?>
     </ul>
   </div>
 </nav>
+<div class="container">
+
+  <h1 id="click_for_stats">For your monitoring...</h1>
+  <div id="monitor">
+    <div id="calendar">
+    </div>
+
+    <div id="stats">
+      <h2>Statistics</h2>
+    </div>
+  </div>
+
+  <!-- jQuery out into separate file -->
+  <script>
+    //slide Toggle to hide/show monitoring menu
+    $(document).ready(function(){
+      $("#click_for_stats").click(function(){
+        $("#monitor").slideToggle();
+      });
+    });
+  </script>
+
+  <!-- javascript put into separate file -->
+  <script type="text/javascript" defer>
+    "use strict";
+    var curDay = new Date();
+
+    document.getElementById("calendar").innerHTML = createCalendar(curDay);
+    //function that created the clanedar and writes the <table>
+    function createCalendar(date) {
+
+      var calendarHTML = "<table id='calendar_table'>";
+      calendarHTML += caption(date);
+      calendarHTML += tableHead();
+      calendarHTML += tableRow(date);
+      calendarHTML += "</table>";
+      return calendarHTML;
+    }
+
+    //function that prints the caption depending on the current month and year
+    function caption(date) {
+      var monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      
+      var curMonth = date.getMonth();
+      
+      var curYear = date.getFullYear();
+      
+      var str = "<caption>" + monthName[curMonth] + " " + curYear + "</caption>";
+      
+      return str;
+    }
+
+    // function that prints the table head
+    function tableHead() {
+      var days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+      var row = "<tr>";
+      
+      for(var i = 0; i < days.length; i++) {
+        row += "<th class='calendar_weekdays'>" + days[i] + "</th>";
+      }
+      row += "</tr>";
+      
+      return row;
+    }
+
+    // function to determine the length of the month
+    function daysInMonth(date) {
+      var dayCount = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+      
+      var curYear = date.getFullYear();
+      var curMonth = date.getMonth();
+      
+      // to determine the lead year and 29 days in February
+      if(curYear % 4 == 0) { 
+        if( (curYear % 100 != 0) || (curYear % 400 == 0) ) {
+          dayCount[1] = 29;	
+        }
+      }
+      
+      return dayCount[curMonth];
+    }
+
+    // prints the table rows and days in month
+    function tableRow(date) {
+      var day = new Date(date.getFullYear(), date.getMonth(), 1);
+      var weekDay = day.getDay();  
+      
+      var html = "<tr>";
+      for(var i = 0; i < weekDay; i++) {
+        html += "<td></td>";
+      }
+      
+      
+      var totalDays = daysInMonth(date);
+      
+      var today = date.getDate();
+      
+      for(var i = 1; i <= totalDays; i++) {
+        day.setDate(i);
+        weekDay = day.getDay();
+        
+        // starts new row if the weekday is Sun
+        if(weekDay === 0){
+          html += "<tr>";
+        }
+        // special condition to highlight the current day through css
+        if(i === today) {
+          html += "<td class='calendar_dates' id='calendar_today'>" + i + "</td>";
+        } else {
+          html += "<td class='calendar_dates'>" + i + "</td>";
+        }
+        
+        // finishes the row if the weekday is Sat
+        if(weekDay === 6){
+          html += "</tr>";
+        }
+
+        
+      }
+      
+      return html;
+      
+    }
+    </script>
 
 <?php
 }?>
 
   <?php
-
+// function that creates the table based on the db reading
 function listReservations($reservation){
   ?>
 
-
-
-  <h1>Manage reservations</h1>
 
   <?php
   if ($reservation == null) {
@@ -58,6 +182,7 @@ function listReservations($reservation){
   else{
   ?>
   <table class="table ">
+    <caption>Manage reservations</caption>
     <thead>
       <tr>
         <th>Reservation #</th>
@@ -93,11 +218,13 @@ function listReservations($reservation){
     </tbody>
 
   </table>  
-  
+  </div>
 </body>
 </html>
 <?php
 }
+
+//calling all the php functions
 
 heading();
 
