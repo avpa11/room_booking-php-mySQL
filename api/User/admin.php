6bececca3c1343ff49b561fe9ebd85e17c780077
avpa11 <alinavpak@gmail.com>
2019-03-31 21:53:41 -0700

    update and search

--- api/User/admin.php
+++ api/User/admin.php
@@ -23,6 +23,7 @@ function heading() { ?>
   <link rel="stylesheet" href="../../assets/css/reservation.css" />
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>  
+  <script src="../../assets/js/monthlycalendar.js"></script>  
   
 </head>
 <body>
@@ -44,122 +45,23 @@ function heading() { ?>
     </div>
 
     <div id="stats">
-      <h2>Statistics</h2>
+      <h2>Search</h2>
+      <?php searchForm(); ?>
     </div>
   </div>
 
-  <!-- jQuery out into separate file -->
   <script>
+
+  document.getElementById("calendar").innerHTML = createCalendar(curDay);
+
     //slide Toggle to hide/show monitoring menu
     $(document).ready(function(){
       $("#click_for_stats").click(function(){
         $("#monitor").slideToggle();
       });
     });
+
   </script>
-
-  <!-- javascript put into separate file -->
-  <script type="text/javascript" defer>
-    "use strict";
-    var curDay = new Date();
-
-    document.getElementById("calendar").innerHTML = createCalendar(curDay);
-    //function that created the clanedar and writes the <table>
-    function createCalendar(date) {
-
-      var calendarHTML = "<table id='calendar_table'>";
-      calendarHTML += caption(date);
-      calendarHTML += tableHead();
-      calendarHTML += tableRow(date);
-      calendarHTML += "</table>";
-      return calendarHTML;
-    }
-
-    //function that prints the caption depending on the current month and year
-    function caption(date) {
-      var monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-      
-      var curMonth = date.getMonth();
-      
-      var curYear = date.getFullYear();
-      
-      var str = "<caption>" + monthName[curMonth] + " " + curYear + "</caption>";
-      
-      return str;
-    }
-
-    // function that prints the table head
-    function tableHead() {
-      var days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
-      var row = "<tr>";
-      
-      for(var i = 0; i < days.length; i++) {
-        row += "<th class='calendar_weekdays'>" + days[i] + "</th>";
-      }
-      row += "</tr>";
-      
-      return row;
-    }
-
-    // function to determine the length of the month
-    function daysInMonth(date) {
-      var dayCount = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-      
-      var curYear = date.getFullYear();
-      var curMonth = date.getMonth();
-      
-      // to determine the lead year and 29 days in February
-      if(curYear % 4 == 0) { 
-        if( (curYear % 100 != 0) || (curYear % 400 == 0) ) {
-          dayCount[1] = 29;	
-        }
-      }
-      
-      return dayCount[curMonth];
-    }
-
-    // prints the table rows and days in month
-    function tableRow(date) {
-      var day = new Date(date.getFullYear(), date.getMonth(), 1);
-      var weekDay = day.getDay();  
-      
-      var html = "<tr>";
-      for(var i = 0; i < weekDay; i++) {
-        html += "<td></td>";
-      }
-      
-      
-      var totalDays = daysInMonth(date);
-      
-      var today = date.getDate();
-      
-      for(var i = 1; i <= totalDays; i++) {
-        day.setDate(i);
-        weekDay = day.getDay();
-        
-        // starts new row if the weekday is Sun
-        if(weekDay === 0){
-          html += "<tr>";
-        }
-        // special condition to highlight the current day through css
-        if(i === today) {
-          html += "<td class='calendar_dates' id='calendar_today'>" + i + "</td>";
-        } else {
-          html += "<td class='calendar_dates'>" + i + "</td>";
-        }
-        
-        // finishes the row if the weekday is Sat
-        if(weekDay === 6){
-          html += "</tr>";
-        }
-
-        
-      }
-      
-      return html;
-      
-    }
-    </script>
 
 <?php
 }?>
@@ -172,11 +74,23 @@ function listReservations($reservation){
 
   <?php
   if ($reservation == null) {
-    ?>
-    <div>
-      <h2>There are no records in the database</h2>
-    </div>
-    <?php
+
+    if (!empty($_GET['search']))
+            {
+                ?>
+                <div class="box content">
+                    <H5>No results found for "<?php echo $_GET['search']; ?>"</H5>
+                </div>
+            <?php
+            }
+            else
+            {
+                ?>
+                <div class="box content">
+                    <H5>No reservations in the database</H5>
+                </div>
+                <?php
+            }
   }
 
   else{
@@ -209,7 +123,7 @@ function listReservations($reservation){
         <TD>'.$r->date.'</TD>
         <TD>'.$r->start_time.'</TD>
         <TD>'.$r->end_time.'</TD>
-        <TD><a id="update" href = "update.php">Update</a></TD>
+        <TD><a id="update" href = "?action=update&reservation_id='.$r->reservation_id.'">Update</a></TD>
         <TD><a id="delete" href = "?action=delete&reservation_id='.$r->reservation_id.'">Delete</a></TD>
         </TR>';
       }
@@ -224,14 +138,103 @@ function listReservations($reservation){
 <?php
 }
 
+function updateReservation($reservation)
+    {
+
+      $resev = new reservation();
+
+        // Verifies if there is any POST data
+        if (!empty($_POST))
+        {
+            // Verifies if the ENTIRE FORM was filled in, and if so, creates the new reservation
+            if (!empty($_POST['reservation_id']) && !empty($_POST['description']) && !empty($_POST['number_of_people'])
+            && !empty($_POST['date']) && !empty($_POST['start_time']) && !empty($_POST['end_time']))
+            {
+              $resev->update($_POST); 
+            }
+        }
+
+        // Creates an array from which to get the reservation's data
+        foreach ($reservation as $attribute)
+        {
+            $myReservation = ['reservation_id' =>$attribute->reservation_id,
+            //'stud_id'=>$attribute->stud_id,
+            //'room_id'=>$attribute->room_id,
+            'description'=>$attribute->description,
+            'number_of_people'=>$attribute->number_of_people,
+            'date'=>$attribute->date,
+            'start_time'=>$attribute->start_time,
+            'end_time'=>$attribute->end_time];
+        }
+        ?>
+        <div class="box content">
+        <FORM METHOD="POST" ACTION="">
+            <DIV CLASS="form-group">
+                <INPUT TYPE="hidden" NAME="reservation_id" ID="reservation_id" VALUE="<?php echo $myReservation['reservation_id'] ?>" />
+                <LABEL FOR="description">Description</LABEL>
+                <INPUT TYPE="text" CLASS="form-control" NAME="description" ID="description" ARIA-DESCTIBEDBY="description_help" VALUE = "<?php echo $myReservation['description']; ?>">
+            </DIV>
+
+            <DIV CLASS="form-group">
+                  <LABEL FOR="number_of_people">Number of People</LABEL>
+                  <INPUT TYPE="text" CLASS="form-control" NAME="number_of_people" maxlength="1" ID="number_of_people" ARIA-DESCTIBEDBY="number_of_people_help" VALUE = "<?php echo $myReservation['number_of_people']; ?>">
+            </DIV>
+
+            <DIV CLASS="form-group">
+                <LABEL FOR="date">Date</LABEL>
+                <INPUT TYPE="date" CLASS="form-control" NAME="date" ID="date" ARIA-DESCTIBEDBY="date_help" VALUE = "<?php echo $myReservation['date']; ?>">
+            </DIV>
+
+            <DIV CLASS="form-group">
+                  <LABEL FOR="start_time">Start Time</LABEL>
+                  <INPUT TYPE="text" CLASS="form-control" NAME="start_time" ID="start_time" ARIA-DESCTIBEDBY="start_time_help" VALUE = "<?php echo $myReservation['start_time']; ?>">
+            </DIV>
+
+            <DIV CLASS="form-group">
+                  <LABEL FOR="end_time">End Time</LABEL>
+                  <INPUT TYPE="text" CLASS="form-control" NAME="end_time" maxlength="1" ID="end_time" ARIA-DESCTIBEDBY="end_time_help" VALUE = "<?php echo $myReservation['end_time']; ?>">
+            </DIV>
+
+        
+            <INPUT CLASS="btn btn-primary" TYPE="SUBMIT" VALUE="Update Reservation ">
+        </DIV>
+        <?php 
+  }
+
+
+// the search form
+function searchForm() { ?>
+  <form action = "" method = "GET">
+      <div>
+          <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" id = "search" name = "search" />
+          
+      </div>
+      <input class="button" type = "submit" value = "Search" />
+  </form>
+<?php
+}
+
 //calling all the php functions
 
 heading();
 
 if (empty($_GET))
 {
-
   listReservations($resev->read());
+}
+
+else if (!empty($_GET))
+{
+
+  if (!empty($_GET['search']))
+  {
+      listReservations($resev->search($_GET['search']));
+  }
+  // If no search term(s) were provided,  renders the complete PLAYERS table
+  else
+  {
+      listReservations($resev->read());
+  }  
 }
 
 else if (isset($_GET['action']))
@@ -239,9 +242,13 @@ else if (isset($_GET['action']))
   switch ($_GET['action'])
   {
     case 'delete':
-    $resev->delete($_GET['reservation_id']);
-    listReservations($resev->read());
-    break;
+      $resev->delete($_GET['reservation_id']);
+      listReservations($resev->read());
+      break;
+
+    case 'update':
+      updateReservation($resev->listOne($_GET['reservation_id']));
+      break;
   }
 }
 ?>
